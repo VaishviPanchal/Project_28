bac3f7034135f396a1e0533fb721676a8e16cbc5
VaishviPanchal <78603593+VaishviPanchal@users.noreply.github.com>
2021-06-28 20:26:40 +0530

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,26 @@
+class Launcher
+{
+    constructor (bodyA,bodyB){
+        var options={
+			bodyA : bodyA,
+            pointB : bodyB,
+            length : 50,
+            stiffness : 13,	
+		}
+		this.body=Constraint.create(options);
+ 		World.add(world,this.body);
+	}
+	display(){
+		var bodyA = this.body.bodyA.position;
+		var bodyB = this.body.pointB;
+		
+		push()
+		stroke(0)
+		strokeWeight(4)
+		line(bodyA.x,bodyA.y,bodyB.x,bodyB.y)
+		pop();
+    }  
+	fly(){
+		this.body.bodyA=null;
+	}                    
+}
