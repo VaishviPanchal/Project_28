c072a5021649375017493d8af513dfe2300ebefc
VaishviPanchal <78603593+VaishviPanchal@users.noreply.github.com>
2021-07-02 22:15:45 +0530

    Update Launcher.js

--- js/Launcher.js
+++ js/Launcher.js
@@ -1,26 +1,32 @@
+
 class Launcher
 {
     constructor (bodyA,bodyB){
         var options={
-			bodyA : bodyA,
+	    bodyA : bodyA,
             pointB : bodyB,
-            length : 50,
-            stiffness : 0.05,	
+            length : 5,
+            stiffness : 0.1,	
 		}
 		this.body=Constraint.create(options);
  		World.add(world,this.body);
 	}
 	display(){
-		var bodyA = this.body.bodyA.position;
-		var bodyB = this.body.pointB;
+		if(this.body.bodyA)
+		{var bodyA = this.body.bodyA.position;
+			var bodyB = this.body.pointB;
+			
+			push()
+			stroke(0)
+			strokeWeight(2)
+			line(bodyA.x,bodyA.y,bodyB.x,bodyB.y)
+			pop();}
 		
-		push()
-		stroke(0)
-		strokeWeight(4)
-		line(bodyA.x,bodyA.y,bodyB.x,bodyB.y)
-		pop();
     }  
 	fly(){
 		this.body.bodyA=null;
-	}                    
+	}
+	attach(body){
+		this.body.bodyA=body
+	}
 }
